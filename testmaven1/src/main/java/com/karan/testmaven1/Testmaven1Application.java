b321e5766362fd35251d6ed3aeade428a3186f13
kkn2k19 <karankumarnonia11@gmail.com>
2025-02-25 00:08:39 +0530

    Day 1 : TestMaven1

---
+++ testmaven1/src/main/java/com/karan/testmaven1/Testmaven1Application.java
@@ -0,0 +1,13 @@
+package com.karan.testmaven1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testmaven1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testmaven1Application.class, args);
+	}
+
+}
---
+++ testmaven1/src/test/java/com/karan/testmaven1/Testmaven1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karan.testmaven1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testmaven1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
