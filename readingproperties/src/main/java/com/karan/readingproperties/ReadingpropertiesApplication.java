8fd218f07ab2db244eb8442eeeb4659eb92223f3
kkn2k19 <karankumarnonia11@gmail.com>
2025-02-24 20:15:48 +0530

    Demo Try for GIT UPs

---
+++ readingproperties/src/main/java/com/karan/readingproperties/ReadingpropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.karan.readingproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingpropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingpropertiesApplication.class, args);
+	}
+
+}
